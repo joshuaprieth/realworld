66baf4a89c7e62f86d1e471cbbab1258ea4194ee
Joshua Prieth <198204205+joshuaprieth@users.noreply.github.com>
2025-02-07 12:17:46 +0100

    Create a persistent database

--- src/database.rs
+++ src/database.rs
@@ -5,22 +5,8 @@ pub use sqlx::sqlite::SqlitePool as Pool;
 pub async fn connect() -> Result<Pool, sqlx::Error> {
     let pool = SqlitePoolOptions::new()
         .max_connections(1)
-        .connect("sqlite::memory:")
+        .connect("sqlite:realworld.db")
         .await?;
-
-    sqlx::query(
-        "
-            CREATE TABLE `users` (
-                email VARCHAR(32) NOT NULL UNIQUE,
-                password VARCHAR(64) NOT NULL,
-                username VARCHAR(32) NOT NULL UNIQUE,
-                bio VARCHAR(256) NULL,
-                image VARCHAR(256) NULL
-            )
-        ",
-    )
-    .execute(&pool)
-    .await?;
 
     Ok(pool)
 }
