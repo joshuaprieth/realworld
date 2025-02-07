e5de9bf9586cf94f792b29fee1ba6fef9a5dbbf5
Joshua Prieth <198204205+joshuaprieth@users.noreply.github.com>
2025-02-07 12:17:46 +0100

    Create a submodule for authentication

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,11 @@
+mod auth;
+
+use auth::{authentication, get_current_user};
 use axum::{
-    extract::State,
     routing::{get, post},
-    Json, Router,
+    Router,
 };
-use axum_extra::headers;
-use axum_extra::TypedHeader;
-use headers::{Header, HeaderName, HeaderValue};
-use serde::{Deserialize, Serialize};
-use std::{collections::HashMap, iter, sync::Arc};
+use std::sync::Arc;
 
 #[tokio::main]
 async fn main() {
@@ -24,91 +22,3 @@ async fn main() {
 
 #[derive(Debug)]
 struct AppState {}
-
-#[derive(Debug, Clone, PartialEq, Eq, Hash)]
-struct Token(String);
-
-static TOKEN: HeaderName = HeaderName::from_static("authorization");
-
-impl Header for Token {
-    fn name() -> &'static HeaderName {
-        &TOKEN
-    }
-
-    fn decode<'i, I>(values: &mut I) -> Result<Self, headers::Error>
-    where
-        I: Iterator<Item = &'i HeaderValue>,
-    {
-        let first = values.next().ok_or_else(headers::Error::invalid)?;
-        let string = first.to_str().map_err(|_| headers::Error::invalid())?;
-
-        if string.starts_with("Token ") {
-            Ok(Token(string[6..].to_string()))
-        } else {
-            Err(headers::Error::invalid())
-        }
-    }
-
-    fn encode<E: Extend<HeaderValue>>(&self, values: &mut E) {
-        values.extend(iter::once(
-            HeaderValue::from_str(&format!("Token {}", self.0)).unwrap(),
-        ));
-    }
-}
-
-#[derive(Debug, Clone, Serialize)]
-struct User {
-    email: String,
-    token: String,
-    username: String,
-    bio: String,
-    image: Option<String>,
-}
-
-#[derive(Debug, Serialize)]
-struct ResponseUser {
-    user: User,
-}
-
-#[derive(Debug, Deserialize)]
-struct Authentication {
-    user: AuthenticationUser,
-}
-
-#[derive(Debug, Deserialize)]
-struct AuthenticationUser {
-    email: String,
-    password: String,
-}
-
-async fn authentication(
-    State(state): State<Arc<AppState>>,
-    Json(authenticate): Json<Authentication>,
-) -> Json<ResponseUser> {
-    println!("{:#?}", authenticate);
-
-    Json(ResponseUser {
-        user: User {
-            email: String::from("jake@jake.jake"),
-            token: String::from("jwt.token.here"),
-            username: String::from("jake"),
-            bio: String::from("I work at statefarm"),
-            image: None,
-        },
-    })
-}
-
-async fn get_current_user(
-    State(state): State<Arc<AppState>>,
-    TypedHeader(token): TypedHeader<Token>,
-) -> Json<ResponseUser> {
-    Json(ResponseUser {
-        user: User {
-            email: String::from("jake@jake.jake"),
-            token: String::from("jwt.token.here"),
-            username: String::from("jake"),
-            bio: String::from("I work at statefarm"),
-            image: None,
-        },
-    })
-}
