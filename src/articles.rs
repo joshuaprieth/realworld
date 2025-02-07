91f3e7f88d01bd4f63e4ddeaa85d60a46760d500
Joshua Prieth <198204205+joshuaprieth@users.noreply.github.com>
2025-02-07 12:17:47 +0100

    Implement the articles feed endpoint

--- src/articles.rs
+++ src/articles.rs
@@ -256,6 +256,131 @@ pub async fn list_articles(
     })
 }
 
+#[derive(Debug, Deserialize)]
+pub struct FeedArticlesConstraints {
+    limit: Option<i64>,
+    offset: Option<i64>,
+}
+
+pub async fn feed_articles(
+    State(app): State<Arc<AppState>>,
+    Auth(user_id): Auth,
+    Query(query): Query<FeedArticlesConstraints>,
+) -> Json<ResponseMultipleArticles> {
+    if query.offset.is_some() && query.limit.is_none() {
+        panic!("Offset must be used with limit");
+    };
+
+    let sql = format!(
+        "
+        SELECT `articles`.`id`, `slug`, `title`, `description`,
+            strftime('%Y-%m-%dT%H:%M:%fZ', `createdAt`) AS `createdAt`,
+            strftime('%Y-%m-%dT%H:%M:%fZ', `updatedAt`) AS `updatedAt`,
+        `author`, (
+            SELECT COUNT(*)
+            FROM `favorites`
+            WHERE `favorites`.`source`=? AND `favorites`.`target`=`articles`.`id`
+        ) AS `favorited`, (
+            SELECT COUNT(*)
+            FROM `favorites`
+            WHERE `target`=`articles`.`id`
+        ) AS `favoritesCount`
+        FROM `articles`
+        JOIN `follows` ON `follows`.`target`=`articles`.`author`
+        WHERE `follows`.`source`=?
+        ORDER BY `updatedAt` DESC
+        {}
+        {}
+    ",
+        query.limit.map(|_| "LIMIT ?").unwrap_or(""),
+        query.offset.map(|_| "OFFSET ?").unwrap_or("")
+    );
+
+    // Get the list of article attributes first
+    let statement = sqlx::query_as::<_, SimpleNoBodyArticle>(&sql)
+        // Use an `id` which never exists if the user is not authenticated
+        .bind(user_id)
+        .bind(user_id);
+
+    let statement = if let Some(limit) = query.limit {
+        statement.bind(limit)
+    } else {
+        statement
+    };
+
+    let statement = if let Some(offset) = query.offset {
+        statement.bind(offset)
+    } else {
+        statement
+    };
+
+    let list = statement.fetch_all(&app.db).await.unwrap();
+
+    // Then fetch the taglist
+    let mut tag_list = Vec::with_capacity(list.len());
+
+    for i in &list {
+        let tags: Vec<String> = sqlx::query_scalar(
+            "
+                SELECT `name`
+                FROM `taglist` INNER JOIN `tags` ON `taglist`.`tag`=`tags`.`id`
+                WHERE `article`=?
+            ",
+        )
+        .bind(i.id)
+        .fetch_all(&app.db)
+        .await
+        .unwrap();
+        tag_list.push(tags);
+    }
+
+    // Then the authors
+    let mut author_list = Vec::with_capacity(list.len());
+
+    for i in &list {
+        let profile = sqlx::query_as::<_, crate::database::Profile>(
+            "
+            SELECT `username`, `bio`, `image`, (
+                SELECT COUNT(*)
+                FROM `follows`
+                WHERE `follows`.`source`=? AND `follows`.`target`=`users`.`id`
+            ) AS `following`
+            FROM `users`
+            WHERE `users`.`id`=?
+        ",
+        )
+        .bind(user_id)
+        .bind(i.author)
+        .fetch_one(&app.db)
+        .await
+        .unwrap();
+
+        author_list.push(profile);
+    }
+
+    // Combine the query results
+    let mut articles = Vec::with_capacity(list.len());
+
+    for ((article, tag_list), author) in iter::zip(iter::zip(list, tag_list), author_list) {
+        articles.push(NoBodyArticle {
+            slug: article.slug,
+            title: article.title,
+            description: article.description,
+            tag_list,
+            created_at: article.created_at,
+            updated_at: article.updated_at,
+            favorited: article.favorited,
+            favorites_count: article.favorites_count,
+            author,
+        });
+    }
+
+    Json(ResponseMultipleArticles {
+        articles_count: articles.len(),
+        articles,
+    })
+}
+
 #[derive(Debug, FromRow)]
 #[sqlx(rename_all = "camelCase")]
 pub struct SimpleBodyArticle {
