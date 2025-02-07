5db84b5e5f04e5f853cc617133fcaa4104e61d6f
Joshua Prieth <198204205+joshuaprieth@users.noreply.github.com>
2025-02-07 12:17:46 +0100

    Create the migration for the articles table

---
+++ migrations/20250128174341_create-articles-table.sql
@@ -0,0 +1,11 @@
+CREATE TABLE IF NOT EXISTS `articles` (
+    `id` INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL UNIQUE,
+    `slug` TEXT NOT NULL UNIQUE,
+    `title` TEXT NOT NULL,
+    `description` TEXT NOT NULL,
+    `body` TEXT NOT NULL,
+    `createdAt` TEXT NOT NULL,
+    `updatedAt` TEXT NOT NULL,
+    `author` INTEGER NOT NULL UNIQUE,
+    FOREIGN KEY (`author`) REFERENCES `users`(`id`)
+)
